01d6eab50fa028003f2e26eba1f7e312e1af36ce
Chan Jun Shern <chanjunshern@gmail.com>
2017-01-07 00:04:22 +0000

    Simple snowfall animation working

--- sketch.js
+++ sketch.js
@@ -1,7 +1,51 @@
-function setup() {
 
+var particles = []; 
+var numParticles = 20;
+
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+
+    for (var i=0; i<numParticles; i++) {
+    	particles.push(new Particle(i));
+    }
+    indexCount = numParticles;
 }
 
 function draw() {
-  
+    background(0,50);
+
+    fill(255);
+    noStroke();
+    for (var i=0; i<numParticles; i++) {
+    	particles[i].display();
+    	particles[i].fall();
+    	particles[i].melt();
+    }
 }
+
+function Particle(index) {
+	this.x = random(width);
+	this.y = random(height)-height;
+	this.index = index;
+	this.diameter = random(10, 30);
+
+	this.display = function() {
+		ellipse(this.x, this.y, this.diameter, this.diameter);
+	}
+
+	this.fall = function() {
+		this.y = constrain(this.y+1, -height, height-this.diameter/2);
+	}
+
+	this.melt = function() {
+		if (this.y == height-this.diameter/2 && random(100)>90) {
+			this.diameter = this.diameter-1;
+		}
+		if (this.diameter <= 0) {
+			// Reset particle
+			particles[this.index] = new Particle(this.index);
+		}
+	}
+}
+
+
